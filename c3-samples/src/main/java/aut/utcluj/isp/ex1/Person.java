0ec372ec3fa8707ee8e8729e2c4e2742f345a589
maria-cretiu-30125 <oanacretiu1@gmail.com>
2021-05-11 18:16:44 +0300

    teeeeest

--- c3-samples/src/main/java/aut/utcluj/isp/ex1/Person.java
+++ c3-samples/src/main/java/aut/utcluj/isp/ex1/Person.java
@@ -1,5 +1,5 @@
 package aut.utcluj.isp.ex1;
-
+//111111
 public class Person {
     private final String firstName;
     private final String lastName;
@@ -7,20 +7,14 @@ public class Person {
     public Person(String firstName) {
         this.firstName = firstName;
         this.lastName = "";
-<<<<<<< HEAD
+
     }
 
     public Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-=======
-        this.firstName= firstName;
-    }
 
-    public Person(String firstName, String lastName) {
-        this.firstName= firstName;
-        this.lastName=lastName;
->>>>>>> 8ca524bde9dd887504f28776b491f43c3254c107
+       
     }
 
     public String getFirstName() {
